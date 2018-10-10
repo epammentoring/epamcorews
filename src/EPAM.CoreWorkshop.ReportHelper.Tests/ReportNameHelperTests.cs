24e7f4019ab0e0b1c050046b771b144f3c9d4cb1
Mihail Romanov <Mihail_Romanov@live.ru>
2018-10-10 20:19:32 +0400

    HOL1. Add pause to test start up

--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -1,5 +1,8 @@
+// <using>
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+// </using>
 
 namespace EPAM.Core.ReportHelper.Tests
 {
@@ -16,5 +19,14 @@ namespace EPAM.Core.ReportHelper.Tests
 
 			result.Should().BeEquivalentTo(expected);
 		}
+
+		// <init>
+		[AssemblyInitialize]
+		public static void Init(TestContext context)
+		{
+			Thread.Sleep(30 * 1000);
+		}
+		// </init>
+
 	}
 }
