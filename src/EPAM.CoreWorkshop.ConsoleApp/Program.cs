6c4061dc9bf132e3e261f8d4e87d04d9cc34a9b8
Mihail Romanov <Mihail_Romanov@live.ru>
2018-10-13 08:20:48 +0400

    1

---
+++ src/EPAM.CoreWorkshop.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace EPAM.CoreWorkshop.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var builder = new WebHostBuilder();
+			builder.UseStartup<Startup>();
+			builder.UseKestrel();
+			builder.UseUrls("http://*:801");
+
+			var host = builder.Build();
+
+			host.Run();
+		}
+	}
+
+	internal class Startup
+	{
+		public void Configure(IApplicationBuilder appBuilder)
+		{
+			appBuilder.Run((c) => {
+				return c.Response.WriteAsync("Hello");
+				
+			});
+		}
+	}
+}
--- src/EPAM.CoreWorkshop.ReportHelper/ArrayHelper.cs
+++ src/EPAM.CoreWorkshop.ReportHelper/ArrayHelper.cs
@@ -2,19 +2,20 @@
 
 namespace EPAM.Core.ReportHelper
 {
-    public static class ArrayHelper
-    {
-        public static void Fill<T>(T[] array, T value)
-        {
+	public static class ArrayHelper
+	{
+		public static void Fill<T>(T[] array, T value)
+		{
 #if NETCOREAPP2_0 || NETCOREAPP2_1
-            Array.Fill<T>(array, value);
+			Array.Fill<T>(array, value);
 #else
-            for (var i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
-            {
-                array[i] = value;
-            }
+			for (var i = array.GetLowerBound(0); 
+				i <= array.GetUpperBound(0); i++)
+			{
+				array[i] = value;
+			}
 #endif
-        }
-    }
+		}
+	}
 }
 
